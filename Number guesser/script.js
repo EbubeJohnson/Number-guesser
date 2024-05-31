fc71c38fb02976424def19c82d85cfdf33fe96ee
EbubeJohnson <ndidiamarikwajohnson@gmail.com>
2024-05-31 16:25:02 +0100

    Add files via upload

---
+++ Number guesser/script.js
@@ -0,0 +1,30 @@
+
+let randomNum = Math.floor(Math.random() * 100) + 1;
+let attempts = 0;
+
+
+const guess = document.getElementById("guess");
+const submit = document.getElementById("submit");
+const hint = document.getElementById("hint");
+const attemptsText = document.getElementById("attempts");
+
+
+submit.addEventListener("click", checkGuess);
+
+
+function checkGuess() {
+  const userValue = Number(guess.value);
+  attempts++;
+
+  
+  if (userValue === randomNum) {
+    hint.textContent = "Congratulations, you guessed it!";
+  } else if (userValue < randomNum) {
+    hint.textContent = "Too low! Try again.";
+  } else {
+    hint.textContent = "Too high! Try again.";
+  }
+
+  // Display the number of attempts
+  attemptsText.textContent = "Attempts: " + attempts;
+}
